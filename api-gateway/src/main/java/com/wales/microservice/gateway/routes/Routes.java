6a66875597aeebca9210719478b0f4dc731a1716
iamwales01 <aolawale@360alumni.com>
2024-10-07 22:21:12 +0100

    implemented circuit breaker for api-gateway and order microservice

--- api-gateway/src/main/java/com/wales/microservice/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/wales/microservice/gateway/routes/Routes.java
@@ -1,70 +1,88 @@
 package com.wales.microservice.gateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.*;
 
+import java.net.URI;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> productsServiceRoutes() {
-        return GatewayRouterFunctions.route("product_service")
+        return route("product_service")
                 .route(
                         // the RequestPredicates has different methods like headers path, GET...
                         // it routes every header or path given to the required url/microservice.
                         RequestPredicates.path("/api/v1/products"),
                         HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> productsServiceRoutesApiDocs() {
-        return GatewayRouterFunctions.route("product_service_swagger")
+        return route("product_service_swagger")
                 .route(
                         RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8080")).filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoutes() {
-        return GatewayRouterFunctions.route("order_service")
+        return route("order_service")
                 .route(
                         RequestPredicates.path("/api/v1/orders"),
                         HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoutesApiDocs() {
-        return GatewayRouterFunctions.route("order_service_swagger")
+        return route("order_service_swagger")
                 .route(
                         RequestPredicates.path("/aggregate/order-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8081")).filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> inventoriesServiceRoutes() {
-        return GatewayRouterFunctions.route("inventory_service")
+        return route("inventory_service")
                 .route(
                         RequestPredicates.path("/api/v1/inventories"),
                         HandlerFunctions.http("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
 
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoutesApiDocs() {
-        return GatewayRouterFunctions.route("inventory_service_swagger")
+        return route("inventory_service_swagger")
                 .route(
                         RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8082")).filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallBackRoute() {
+        return route("fallbackRoute").GET("/fallbackRoute",
+                request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Unavailable, please try again later"))
                 .build();
     }
 }
--- order-service/src/main/java/com/wales/microservice/order/client/InventoryClientWithRestClient.java
+++ order-service/src/main/java/com/wales/microservice/order/client/InventoryClientWithRestClient.java
@@ -1,10 +1,25 @@
 package com.wales.microservice.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClientWithRestClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClientWithRestClient.class);
+
     @GetExchange("/api/v1/inventories")
+    // the name inventory should match with what was provided in the application.properties file
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason {}", code, throwable.getMessage());
+        return false;
+    }
 }
